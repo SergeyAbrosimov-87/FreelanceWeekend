095bd4174ca8cf3fee6751f50e0a6a00701b8873
sergey.abrosimov <sergey.abrosimov@bk.ru>
2024-10-08 15:18:43 +0300

    New Tests and ParameterizedTest

--- src/test/java/ru/netology/services/FreelanceWeekend/services/FreelanceWeekendTest.java
+++ src/test/java/ru/netology/services/FreelanceWeekend/services/FreelanceWeekendTest.java
@@ -1,19 +1,61 @@
 package ru.netology.services.FreelanceWeekend.services;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 public class FreelanceWeekendTest {
-
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/testdata.csv")
-    public void testCashFreelanceWeekend(int expected, int income, int expenses, int threshold) {
+    @Test
+    public void testFreelanceWeekend1() {
         FreelanceWeekend service = new FreelanceWeekend();
 
+        int income = 10000;
+        int expenses = 3000;
+        int threshold = 20000;
+
+        int expected = 3;
         int actual = service.calculate(income, expenses, threshold);
+
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void testFreelanceWeekend2() {
+        FreelanceWeekend service = new FreelanceWeekend();
+
+        int income = 100000;
+        int expenses = 60000;
+        int threshold = 150000;
+
+        int expected = 2;
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "10000,3000,20000,3",
+            "100000,60000,150000,2"
+    })
+    public void testFreelanceWeekendParam1(int income, int expenses, int threshold, int expected) {
+        FreelanceWeekend service = new FreelanceWeekend();
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/testdata.csv")
+    public void testFreelanceWeekendParam2(int income, int expenses, int threshold, int expected) {
+        FreelanceWeekend service = new FreelanceWeekend();
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
